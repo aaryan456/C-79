47206ec6fc05b389ea8875c65642b7af4e1ae48e
aaryan456 <62750261+aaryan456@users.noreply.github.com>
2020-10-13 12:33:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { createAppContainer, createSwitchNavigator,} from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import LoginScreen from './screens/LoginScreen';
+import MainScreen from './screens/ExchangeScreen';
+import ExchangeScreen from './screens/MainScreen';
+
+export default class App extends React.Component {
+  render() {
+   return <AppContainer />;
+  }
+}
+
+const TabNavigator = createBottomTabNavigator({
+  MainScreen: { screen: MainScreen },
+  ExchangeScreen: { screen: ExchangeScreen },
+},
+
+);
+
+const AppNavigator = createSwitchNavigator({
+  LoginScreen:{screen:LoginScreen},
+  TabNavigator : {screen : TabNavigator}
+})
+
+const AppContainer = createAppContainer(AppNavigator);
